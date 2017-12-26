99fdecc293f05d92cf27498a2c738cfadf676d89
smartwebdev2017 <bern.oliver.630@yandex.com>
2017-12-26 16:30:03 +0100

    update

--- pfinder/runSpider.py
+++ pfinder/runSpider.py
@@ -7,14 +7,14 @@ from pcarfinder import PcarfinderDB
 
 
 db = PcarfinderDB()
-# os.system("scrapy crawl carmax -a searchterms_str=Porsche")
-# print('*****  Carmax spider was finished! *****')
-# print('***** Updating carmax sold status *****')
-# db.update_carmax_sold_status()
-# print('***** Finished carmax sold status *****')
-#
-# os.system("scrapy crawl rennlist -a searchterms_str=Porsche")
-# print('*****  Rennist spider was finished! *****')
+os.system("scrapy crawl carmax -a searchterms_str=Porsche")
+print('*****  Carmax spider was finished! *****')
+print('***** Updating carmax sold status *****')
+db.update_carmax_sold_status()
+print('***** Finished carmax sold status *****')
+
+os.system("scrapy crawl rennlist -a searchterms_str=Porsche")
+print('*****  Rennist spider was finished! *****')
 
 with open('shortcodes.json') as data_file1:
     state_obj = json.load(data_file1)
@@ -32,7 +32,7 @@ for item in city_obj:
     crawl_str = 'scrapy crawl craigslist -a searchterms_str=%s -a state=%s' % (shortcode, state)
     print(crawl_str)
     os.system(crawl_str)
-    #print('*****  Craigslist spider was finished! *****')
-    #print('***** Updating craigslist sold status *****')
-    #db.update_carmax_sold_status()
-    #print('***** Finished craigslist sold status *****')
+print('*****  Craigslist spider was finished! *****')
+print('***** Updating craigslist sold status *****')
+db.update_carmax_sold_status()
+print('***** Finished craigslist sold status *****')
